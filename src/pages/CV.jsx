24584b4fe59c9eaa020616fc21195919062d2fb2
VolodiaHryhoriev <88433088+VolodiaHryhoriev@users.noreply.github.com>
2022-05-16 22:50:47 +0300

    fixed mobile version

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -15,7 +15,7 @@ const CV = () => {
             <div className={styles.main}>
                 <div className={styles.mainText}>
                     <h4>Hello there,</h4>
-                <h1 >I am <br/> Volodymyr Hryhoriev</h1>
+                <h1>I am <br/> Volodymyr Hryhoriev</h1>
                     <h3><ReactTypingEffect text={"Front-end Developer."}/></h3>
                 </div>
                 <Tilt className="Tilt" options={{max: 20, scale: 1.01,}}><img src={profileImg} alt="profile Image" className={styles.profileLogo}/></Tilt>
