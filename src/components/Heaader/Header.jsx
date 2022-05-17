35908aa01025170bc13dd652ee7afaadf1e0cab7
VolodiaHryhoriev <88433088+VolodiaHryhoriev@users.noreply.github.com>
2022-05-17 09:43:28 +0300

    fixed mobile version

--- src/components/Heaader/Header.jsx
+++ src/components/Heaader/Header.jsx
@@ -4,47 +4,48 @@ import "rsuite/dist/rsuite.min.css";
 import React from "react";
 import {instanceNavBut} from "./../RSUITE components/rsuiteComp"
 import {ButtonToolbar} from "rsuite";
-import {Link, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+import {Link as Link1, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+import {Link as Link2} from "react-router-dom"
 
 const Header = () => {
     return (
         <div>
             <nav>
                 <div className={styles.navLeft}>
-                    <Link className={styles.link} to="main"
+                    <Link1 className={styles.link} to="main"
                           spy={true}
                           smooth={true}
                           offset={1}
                           duration={500}>
                         Home
-                    </Link>
-                <Link className={styles.link} to="skills"
+                    </Link1>
+                <Link1 className={styles.link} to="skills"
                       spy={true}
                       smooth={true}
                       offset={1}
                       duration={500}>
                     Skills
-                </Link>
-                <Link className={styles.link} to="aboutMe"
+                </Link1>
+                <Link1 className={styles.link} to="aboutMe"
                       spy={true}
                       smooth={true}
                       offset={1}
                       duration={500}>
                     About Me
-                </Link>
-                <Link className={styles.link} to="contact"
+                </Link1>
+                <Link1 className={styles.link} to="contact"
                       spy={true}
                       smooth={true}
                       offset={1}
                       duration={500}>
                     Contact
-                </Link>
+                </Link1>
                 </div>
                 <div className={styles.navRight}>
                     <ButtonToolbar>{instanceNavBut}</ButtonToolbar>
-                <Link className={styles.logButt} to={RouteConst.LOGIN}>
+                <Link2 className={styles.logButt} to={RouteConst.LOGIN}>
                     Login
-                </Link>
+                </Link2>
                 </div>
             </nav>
         </div>
