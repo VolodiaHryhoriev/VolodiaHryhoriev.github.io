3f99d159ebcefa32520c0eadf098379a4f77b25f
VolodiaHryhoriev <88433088+VolodiaHryhoriev@users.noreply.github.com>
2022-05-17 09:19:46 +0300

    fixed mobile version

--- src/components/Heaader/Header.jsx
+++ src/components/Heaader/Header.jsx
@@ -1,27 +1,42 @@
 import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
 import { RouteConst } from "../../common/RouteConst";
 import "rsuite/dist/rsuite.min.css";
 import React from "react";
 import {instanceNavBut} from "./../RSUITE components/rsuiteComp"
 import {ButtonToolbar} from "rsuite";
-
+import {Link, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
 
 const Header = () => {
     return (
         <div>
             <nav>
                 <div className={styles.navLeft}>
-                    <Link className={styles.link} to={"#"}>
+                    <Link className={styles.link} to="main"
+                          spy={true}
+                          smooth={true}
+                          offset={1}
+                          duration={500}>
                         Home
                     </Link>
-                <Link className={styles.link} to={"#"}>
+                <Link className={styles.link} to="skills"
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}>
                     Skills
                 </Link>
-                <Link className={styles.link} to={"#"}>
+                <Link className={styles.link} to="aboutMe"
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}>
                     About Me
                 </Link>
-                <Link className={styles.link} to={"#"}>
+                <Link className={styles.link} to="contact"
+                      spy={true}
+                      smooth={true}
+                      offset={1}
+                      duration={500}>
                     Contact
                 </Link>
                 </div>
--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -12,7 +12,7 @@ import Tilt from 'react-tilt'
 const CV = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.main}>
+            <div id="main" className={styles.main}>
                 <div className={styles.mainText}>
                     <h4>Hello there,</h4>
                 <h1>I am <br/> Volodymyr Hryhoriev</h1>
